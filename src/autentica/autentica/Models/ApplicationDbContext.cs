d961af5ebe047dd8f23aa7b942447ca0832bc899
amandaadriene <amanda.admi@hotmail.com>
2021-10-18 22:53:03 -0300

    Criando o BD

---
+++ src/autentica/autentica/Models/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace autentica.Models
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+
+        }
+
+        public  DbSet<Cliente> Clientes{ get; set; }
+
+    }
+}
---
+++ src/autentica/autentica/Models/Cliente.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace autentica.Models
+{
+    [Table("Clientes")] 
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int CPF_CNPJ { get; set; }
+        public string Endereco { get; set; }
+        public string Cidade { get; set; }
+        public int CEP { get; set; }
+        public string UF { get; set; }
+        public string Pais { get; set; }
+        public int  Telefone { get; set; }
+    }
+}
